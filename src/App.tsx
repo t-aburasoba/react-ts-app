02b5002ffff27ec495875ba52cb8027177233536
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-13 22:15:49 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Button, ChakraProvider} from "@chakra-ui/react";
+import {ChakraProvider} from "@chakra-ui/react";
+import theme from "./theme/theme";
+import {BrowserRouter} from "react-router-dom";
+import {Router} from "./router/Router";
 
 function App() {
-  return (
-    <ChakraProvider>
-      <Button>Button</Button>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider theme={theme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </ChakraProvider>
+    );
 }
 
 export default App;
---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,7 @@
+import {memo, VFC} from "react";
+
+export const UserManagement: VFC = memo(() => {
+    return (
+        <p>This is UserManagement page</p>
+    )
+})
