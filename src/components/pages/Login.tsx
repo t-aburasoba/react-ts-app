248957ad4c9f99b441dbea5d57a8bee2b3a5e32b
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-14 14:20:58 +0900

    make login ui

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,20 @@
 import {memo, VFC} from "react";
+import {Box, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
+import {PrimaryButton} from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
     return (
-        <p>This is Login page</p>
+        <Flex align='center' justify='center' height='100vh'>
+            <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
+                <Heading as='h1' size='lg' textAlign='center'>
+                    User management app
+                </Heading>
+                <Divider my={4} />
+                <Stack spacing={3} py={4} px={10}>
+                    <Input placeholder='User id' />
+                    <PrimaryButton>Login</PrimaryButton>
+                </Stack>
+            </Box>
+        </Flex>
     )
 })
