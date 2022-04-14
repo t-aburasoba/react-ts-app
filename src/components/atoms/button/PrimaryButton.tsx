cce637ece8880c1334387dda296e93a370793b35
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-14 14:42:44 +0900

    add login function

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -2,13 +2,17 @@ import {memo, ReactNode, VFC} from "react";
 import {Button} from "@chakra-ui/react";
 
 type Props = {
-    children: ReactNode
+    children: ReactNode;
+    disabled?: boolean;
+    loading?: boolean;
+    onClick: () => void;
 }
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-    const { children } = props
+    const {children, onClick, disabled = false, loading = false} = props
     return (
-        <Button bg='teal.400' color='white' _hover={{ opacity: 0.8 }}>
+        <Button bg='teal.400' color='white' _hover={{opacity: 0.8}} onClick={onClick} isLoading={loading}
+                disabled={disabled || loading}>
             {children}
         </Button>
     )
