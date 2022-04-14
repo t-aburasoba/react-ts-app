2e8113ed655cf863b3a62c4f27b46915fa37c598
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-14 14:51:37 +0900

    add login ui

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,12 @@ import {useCallback, useState} from "react";
 import axios from "axios";
 import {User} from "../types/api/user";
 import {useHistory} from "react-router-dom";
+import {useMessage} from "./useMessage";
 
 export const useAuth = () => {
     const history = useHistory()
+
+    const {showMessage} = useMessage()
 
     const [loading, setLoading] = useState(false)
 
@@ -14,17 +17,18 @@ export const useAuth = () => {
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
             .then((res) => {
                 if (res.data) {
+                    showMessage({title: 'success to login', status: "success"})
                     history.push('/home')
                 } else {
-                    alert('Cannot find user')
+                    showMessage({title: 'cannot find user', status: "error"})
                 }
             })
             .catch(() => {
-                alert('Cannot login')
+                showMessage({title: 'cannot login', status: "error"})
             })
             .finally(() => {
                 setLoading(false)
             })
-    }, [history])
+    }, [history, showMessage])
     return {login, loading}
 }
