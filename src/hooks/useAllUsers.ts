027b77edac87c762f0b5b3079febe3f572b133b0
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-14 15:31:29 +0900

    update user index ui

---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,24 @@
+import {useCallback, useState} from "react";
+import axios from "axios";
+import {User} from "../types/api/user";
+import {useMessage} from "./useMessage";
+
+export const useAllUsers = () => {
+    const {showMessage} = useMessage()
+    const [loading, setLoading] = useState<boolean>(false)
+    const [users, setUsers] = useState<Array<User>>([])
+    const getUsers = useCallback(() => {
+        setLoading(true)
+        axios.get<Array<User>>('https://jsonplaceholder.typicode.com/users')
+            .then((res) => {
+                setUsers(res.data)
+            })
+            .catch(() => {
+                showMessage({title: 'cannot get users data', status: "error"})
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    },[showMessage])
+    return {getUsers, loading, users}
+}
