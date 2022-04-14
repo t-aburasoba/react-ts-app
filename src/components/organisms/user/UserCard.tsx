b98188969f36145274d48e9ec14c69c6512f9300
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-14 15:12:27 +0900

    add user card ui

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,21 @@
+import {memo, VFC} from "react";
+import {Box, Image, Stack, Text} from "@chakra-ui/react";
+
+type Props = {
+    imageUrl: string;
+    userName: string;
+    fullName: string;
+}
+
+export const UserCard: VFC<Props> = memo((props) => {
+    const {imageUrl, userName, fullName} = props
+    return (
+        <Box w='260px' h='260px' bg='white' borderRadius='10px' shadow='md' p={4} _hover={{ cursor: 'pointer', opacity: 0.8 }}>
+            <Stack textAlign='center'>
+                <Image boxSize='160px' borderRadius='full' alt={userName} m='auto' src={imageUrl} />
+                <Text fontSize='lg' fontWeight='bold'>{userName}</Text>
+                <Text fontSize='sm' color='gray'>{fullName}</Text>
+            </Stack>
+        </Box>
+    )
+})
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,13 @@
 import {memo, VFC} from "react";
+import {Wrap, WrapItem} from "@chakra-ui/react";
+import {UserCard} from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
     return (
-        <p>This is UserManagement page</p>
+        <Wrap p={{ base: 4, md: 10 }}>
+            <WrapItem>
+                <UserCard imageUrl='https://source.unsplash.com/random' userName="aburasoba" fullName='t.tsubasa' />
+            </WrapItem>
+        </Wrap>
     )
 })
