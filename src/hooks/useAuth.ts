1c93181d82a6a509994f0c98f2452e927662e858
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-14 16:25:20 +0900

    add context user

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import {User} from "../types/api/user";
 import {useHistory} from "react-router-dom";
 import {useMessage} from "./useMessage";
+import {useLoginUser} from "./useLoginUser";
 
 export const useAuth = () => {
     const history = useHistory()
@@ -11,24 +12,26 @@ export const useAuth = () => {
 
     const [loading, setLoading] = useState(false)
 
+    const {setLoginUser} = useLoginUser()
+
     const login = useCallback((id: string) => {
         setLoading(true)
 
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
             .then((res) => {
                 if (res.data) {
+                    setLoginUser(res.data)
                     showMessage({title: 'success to login', status: "success"})
                     history.push('/home')
                 } else {
                     showMessage({title: 'cannot find user', status: "error"})
+                    setLoading(false)
                 }
             })
             .catch(() => {
                 showMessage({title: 'cannot login', status: "error"})
-            })
-            .finally(() => {
                 setLoading(false)
             })
-    }, [history, showMessage])
+    }, [history, showMessage, setLoginUser])
     return {login, loading}
 }
---
+++ src/providers/LoginUserProvider.tsx
@@ -0,0 +1,19 @@
+import {createContext, Dispatch, ReactNode, SetStateAction, useState} from "react";
+import {User} from "../types/api/user";
+
+export type LoginUserContextType = {
+    loginUser: User | null;
+    setLoginUser: Dispatch<SetStateAction<User | null>>
+}
+
+export const LoginUserContext = createContext<LoginUserContextType>({} as LoginUserContextType)
+
+export const LoginUserProvider = (props: {children: ReactNode | null}) => {
+    const {children} = props
+    const [loginUser, setLoginUser] = useState<User | null>(null)
+    return (
+        <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+            {children}
+        </LoginUserContext.Provider>
+    )
+}
